fa2fff7c91d1047059fa83d5bcfee32b8af6c325
parsaljndrn <pars.aljndrn27@gmail.com>
2026-03-26 02:59:03 +0800

    prep for deployment

---
+++ start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Navigate to the folder where app.py is
+cd BoostMatch/system
+
+# Set PORT (Railway provides this automatically)
+export PORT=${PORT:-8080}
+
+# Run the app with uvicorn
+uvicorn app:app --host 0.0.0.0 --port $PORT
