e126f0ce6c4ae0a271d772739bf64842175d6b85
parsaljndrn <pars.aljndrn27@gmail.com>
2026-03-27 03:21:58 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,13 +2,17 @@
 
 echo "🚀 START.SH RUNNING"
 
-# Make sure the Linux FFmpeg binary is executable
-echo "Making FFmpeg executable..."
+# Make sure the Linux FFmpeg and FFprobe binaries are executable
+echo "Making FFmpeg and FFprobe executable..."
 chmod +x ./system/ffmpeg/ffmpeg
+chmod +x ./system/ffmpeg/ffprobe
 
-# Check FFmpeg version to verify it works
+# Check FFmpeg and FFprobe version
 echo "Checking FFmpeg binary..."
 ./system/ffmpeg/ffmpeg -version
+
+echo "Checking FFprobe binary..."
+./system/ffmpeg/ffprobe -version
 
 echo "Moving to app directory..."
 cd BoostMatch/system
